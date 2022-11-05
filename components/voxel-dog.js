666186dc1467352c75906261f9a360f9357a0f65
Saul Fimbres <sfimbres@necodex.com>
2022-11-04 18:40:10 -0700

    v2

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,8 +12,6 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB =
-    (process.env.NODE_ENV === 'production' ? '/public/' : '') + '/dog.glb'
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
